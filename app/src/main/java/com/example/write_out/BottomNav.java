a3e7a63e378fb5a08cc2829cc1bc8d0df73b771e
KiraCodes6103 <21je1072@iitism.ac.in>
2022-03-28 08:00:34 +0530

    Changing fragments and a working floating action button.

--- app/src/main/java/com/example/write_out/BottomNav.java
+++ app/src/main/java/com/example/write_out/BottomNav.java
@@ -1,14 +1,58 @@
 package com.example.write_out;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.write_out.databinding.ActivityBottomNavBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class BottomNav extends AppCompatActivity {
+    FirebaseAuth mFirebase;
+    ActivityBottomNavBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bottom_nav);
+
+
+    binding = ActivityBottomNavBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+    replacefragment(new MyProfile());
+
+    binding.bottomnavigation.setOnItemSelectedListener(item -> {
+        switch (item.getItemId()){
+
+            case R.id.My: replacefragment(new MyProfile());
+                break;
+            case R.id.Other: replacefragment(new OtherArticle());
+                break;
+            case R.id.Favourites:  replacefragment(new Favourite());
+                break;
+            case R.id.Logout_Button: replacefragment(new LogOut_Fragment());
+        }
+
+        return  true;
+    });
     }
+    private void replacefragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.FrameContainer,fragment);
+        fragmentTransaction.commit();
+
+    }
+    public void NewArticle(View view){
+        Intent intent = new Intent(this, NewAricle.class);
+        startActivity(intent);
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/write_out/MyProfile.java
@@ -0,0 +1,56 @@
+package com.example.write_out;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+public class MyProfile extends Fragment {
+    FloatingActionButton floatingActionButton;
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+
+    private String mParam1;
+    private String mParam2;
+
+    public MyProfile() {
+
+    }
+
+
+
+    public static MyProfile newInstance(String param1, String param2) {
+        MyProfile fragment = new MyProfile();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_my_profile, container, false);
+    }
+
+
+}
